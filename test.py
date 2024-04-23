aaf90b730658ec8eb83538d654264725ea0b33d2
onus4 <jasioleksol@gmail.com>
2024-04-23 09:54:36 +0200

    .

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
-print("Hello")
+import random
+import string
+
+print ("Wygeneruje ci losowe, silne haslo!")
+x = int(input("Podaj preferowaną długość hasła"))
+
+haslo=[]
+
+for i in range (x):
+    string.ascii_letters
+    'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*)(-=7'
+    y = random.choice(string.ascii_letters)
+    haslo.append(y)
+
+print(*haslo)
+
+
